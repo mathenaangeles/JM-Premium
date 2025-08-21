524f163b84b3a69bdf4f5dbaecc6d215de8963e2
Mathena Angeles <mathena828@gmail.com>
2025-08-22 04:11:11 +0800

    Refactor axios.js

--- server/config.py
+++ server/config.py
@@ -4,26 +4,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def str_to_bool(value):
+    return str(value).lower() in ("true", "1", "yes")
+
 class Configuration(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-    SQLALCHEMY_ECHO = os.getenv('SQLALCHEMY_ECHO')
-    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
-    FLASK_ENV = os.getenv('FLASK_ENV')
+    SQLALCHEMY_TRACK_MODIFICATIONS = str_to_bool(os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', "false"))
+    SQLALCHEMY_ECHO = str_to_bool(os.getenv('SQLALCHEMY_ECHO', "false"))
+    FLASK_DEBUG = str_to_bool(os.getenv('FLASK_DEBUG', "false"))
+    FLASK_ENV = os.getenv('FLASK_ENV', "production")
+
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_COOKIE_SECURE = os.getenv('JWT_COOKIE_SECURE')
-    JWT_ACCESS_COOKIE_NAME = os.getenv('JWT_ACCESS_COOKIE_NAME')
-    JWT_REFRESH_COOKIE_NAME = os.getenv('JWT_REFRESH_COOKIE_NAME')
-    JWT_COOKIE_CSRF_PROTECT = os.getenv('JWT_COOKIE_CSRF_PROTECT')
+    JWT_COOKIE_SECURE = str_to_bool(os.getenv('JWT_COOKIE_SECURE', "true"))
+    JWT_ACCESS_COOKIE_NAME = os.getenv('JWT_ACCESS_COOKIE_NAME', "access_token_cookie")
+    JWT_REFRESH_COOKIE_NAME = os.getenv('JWT_REFRESH_COOKIE_NAME', "refresh_token_cookie")
+    JWT_COOKIE_CSRF_PROTECT = str_to_bool(os.getenv('JWT_COOKIE_CSRF_PROTECT', "true"))
+
     FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
     XENDIT_API_KEY = os.getenv("XENDIT_API_KEY")
     XENDIT_WEBHOOK_TOKEN = os.getenv("XENDIT_WEBHOOK_TOKEN")
     XENDIT_BASE_URL = os.getenv("XENDIT_BASE_URL", "https://api.xendit.co")
+
     NINJAVAN_BASE_URL = os.getenv("NINJAVAN_BASE_URL")
     NINJAVAN_CLIENT_ID = os.getenv("NINJAVAN_CLIENT_ID")
     NINJAVAN_CLIENT_SECRET = os.getenv("NINJAVAN_CLIENT_SECRET")
     NINJAVAN_WEBHOOK_SECRET = os.getenv("NINJAVAN_WEBHOOK_SECRET")
-    
